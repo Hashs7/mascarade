bb1e7de293ebbbb85d5e85ea745c8380612d64be
amelie laurent <amelielaurent38@gmail.com>
2019-04-28 16:03:33 +0200

    Start student login

---
+++ client/src/components/StudentLogin.vue
@@ -0,0 +1,109 @@
+<template>
+    <div>
+        <v-snackbar
+                v-model="snackbar"
+                :timeout="6000"
+                top
+                right
+        >
+            {{ responseMsg }}
+            <v-btn
+                    color="blue"
+                    flat
+                    @click="snackbar = false"
+            >
+                Fermer
+            </v-btn>
+        </v-snackbar>
+
+        <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+        >
+
+            <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+            ></v-text-field>
+
+            <v-text-field
+                    v-model="password"
+                    label="Password"
+                    required
+                    type="password"
+            ></v-text-field>
+
+            <v-btn
+                    :disabled="!valid"
+                    color="success"
+                    @click="validate"
+            >
+                Connexion
+            </v-btn>
+        </v-form>
+    </div>
+
+</template>
+
+<script>
+    import {ROUTE_STUDENT_LOGIN} from "../utils/constant";
+    import {api} from "../utils/API";
+
+    export default {
+        name: 'StudentLogin',
+        data: () => ({
+            valid: true,
+            snackbar: false,
+            responseMsg: '',
+            email: '',
+            password: '',
+            emailRules: [
+              v => !!v || 'E-mail is required',
+              v => /.+@.+/.test(v) || 'E-mail must be valid'
+            ]
+        }),
+
+        methods: {
+            validate () {
+                if (!this.$refs.form.validate()) return;
+                const data    = {
+                    email: this.email,
+                    password: this.password,
+                };
+                const options = {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    data: JSON.stringify(data),
+                    url: ROUTE_STUDENT_LOGIN
+                };
+
+               api(options)
+                .then(res => {
+                  this.responseMsg = res.data.message;
+                  this.snackbar = true;
+                  setTimeout(() => {
+                    this.$router.push('/student/first')
+                  }, 3000)
+                })
+                .catch(err => {
+                  console.log(err);
+              })
+            },
+            reset () {
+                this.$refs.form.reset()
+            },
+            resetValidation () {
+                this.$refs.form.resetValidation()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+ button {
+    margin-left: 0;
+  }
+</style>
