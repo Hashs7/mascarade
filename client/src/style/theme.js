cab96285357e5af15399d9dd96cf45c47084bc2e
Sebastien Hernoux <seb_her@hotmail.fr>
2019-05-01 13:39:39 +0200

    add theme provider

---
+++ client/src/style/theme.js
@@ -0,0 +1,10 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+    primary: {
+        base: colors.purple.base,
+        darken1: colors.purple.darken2
+    },
+    secondary: colors.indigo,
+    tertiary: colors.deepPurple.base
+}
