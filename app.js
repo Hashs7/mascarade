2204f5958cd8a083004368f5889a57bb7aa3f9f3
Sebastien Hernoux <seb_her@hotmail.fr>
2019-05-13 16:03:20 +0200

    enable cors manually

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const accessLogStream = fs.createWriteStream(
     {flags: 'a'});
 
 // app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(morgan('dev', {stream: accessLogStream}));
 app.use(bodyParser.json());
 app.use(helmet());
