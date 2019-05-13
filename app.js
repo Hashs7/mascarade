914cbecae3c0bfef251290d4e404db9756a13c52
Sebastien Hernoux <seb_her@hotmail.fr>
2019-05-13 15:36:26 +0200

    unsplit app and server js

--- app.js
+++ app.js
@@ -8,15 +8,34 @@ const helmet      = require('helmet');
 const router      = require('./routes');
 const app         = express();
 
+const authStudent = require('./routes/auth/authStudent');
+const authTeacher = require('./routes/auth/authTeacher');
+const session     = require('./routes/session');
+
 const accessLogStream = fs.createWriteStream(
     path.join(__dirname, 'access.log'),
     {flags: 'a'});
 
+app.use(cors());
 app.use(morgan('dev', {stream: accessLogStream}));
 app.use(bodyParser.json());
-app.use(cors());
 app.use(helmet());
 
-router.init(app);
+// router.init(app);
+
+app.use('/auth/student', authStudent);
+app.use('/auth/teacher', authTeacher);
+app.use('/session', session);
+
+// All invalid routes
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = 500;
+    // const status  = error.statusCode || 500;
+    const message = error.message;
+    const data    = error.data;
+    res.status(status).json({message: message, data: data});
+});
+
 
 module.exports = app;
