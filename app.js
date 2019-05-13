e2e3a5bbff380e75480db3a5e9a5fd4cb79e489f
Sebastien Hernoux <seb_her@hotmail.fr>
2019-05-13 16:18:49 +0200

    gneeeee

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path        = require('path');
 const fs          = require('fs');
 const express     = require('express');
 const bodyParser  = require('body-parser');
-// const cors        = require('cors');
+const cors        = require('cors');
 const morgan      = require('morgan');
 const helmet      = require('helmet');
 const router      = require('./routes');
@@ -12,14 +12,14 @@ const accessLogStream = fs.createWriteStream(
     path.join(__dirname, 'access.log'),
     {flags: 'a'});
 
-// app.use(cors());
-app.use((req, res, next) => {
+app.use(cors());
+/*app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-});
+});*/
 
 app.use(morgan('dev', {stream: accessLogStream}));
 app.use(bodyParser.json());
