0f8057deba625d928415ca9590d65f93297ad699
amelie laurent <amelielaurent38@gmail.com>
2019-05-23 21:13:47 +0200

    Informations block

--- client/src/components/traps/FakeNews.vue
+++ client/src/components/traps/FakeNews.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="FakeNews-container">
         <Content />
+        <Informations />
     </div>
 
 </template>
 <script>
     import Content from '@/components/traps/post/Content';
+    import Informations from '@/components/traps/post/Informations';
     export default {
         name: "FakeNews",
-        components: { Content },
+        components: { Content, Informations },
         data: () => ({
           
         }),
---
+++ client/src/components/traps/post/Informations.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="Informations-container">
+        <div class="Informations-group">
+            <div class="Informations-content">
+              <img class="Informations-image" src="../../../assets/logo.png"/>
+              <p class="Informations-title">{{informations[0].title}}</p>
+            </div>
+            <div class="Informations-content">
+              <img class="Informations-image" src="../../../assets/logo.png"/>
+              <p class="Informations-title">{{informations[1].title}}</p>
+            </div>
+            <div class="Informations-content">
+              <img class="Informations-image" src="../../../assets/logo.png"/>
+              <p class="Informations-title">{{informations[2].title}}</p>
+            </div>
+            <div class="Informations-content">
+              <img class="Informations-image" src="../../../assets/logo.png"/>
+              <p class="Informations-title">{{informations[3].title}}</p>
+            </div> 
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Informations",
+        data: () => ({
+          informations: [{
+              title: "Publié par Mr Chafoin",
+              image: "",
+            },{
+              title: "60",
+              image: "",
+            },{
+              title: "12",
+              image: "",
+            },{
+              title: "Il y a 2h",
+              image: "",
+          }]
+        }),
+    }
+</script>
+
+<style scoped lang="scss">
+    .Informations {
+      &-container {
+        background-color: #fff;
+        padding: 2rem;
+      }
+      &-group {
+        display: flex;
+        justify-content: space-between;
+      }
+      &-title {
+        font-weight: bold;
+        font-size: 1rem;
+        margin-bottom: 0;
+        margin-left: 1rem;
+      }
+      &-image {
+        width: 3rem;
+        height: 3rem;
+      }
+      &-content {
+        display: flex;
+        align-items: center;
+      }
+    }
+     
+</style>
