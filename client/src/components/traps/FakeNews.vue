fc20b62db2ae7a883109b5c4fc6f83b1819493be
amelie laurent <amelielaurent38@gmail.com>
2019-05-30 10:46:30 +0200

    Refracto buttons operation

--- client/src/components/traps/FakeNews.vue
+++ client/src/components/traps/FakeNews.vue
@@ -2,18 +2,46 @@
     <div class="FakeNews-container">
         <Content title="Au programme des 6e le chinois !" description="Le ministère de l’éducation nationale et de la jeunesse souhaite ajouter une nouvelle langue au programme scolaire des élèves de 6ème : le chinois !"/>
         <Informations name="Publié par Mr Chafoin" like="12" comment="15" date="Il y a 3 heures" />
-        <ButtonContainer titleFirst="Partager" titleSecond="Signaler" />
+        <div class="Button-container">
+          <div class="Button-group">
+          <button class="Button-share" type="button" id="show-modal" @click="showModal">Partager</button>
+          <button class="Button-report" type="button" @click="updateReport">Signaler</button>
+          <modal v-show="isModalVisible"
+                  @close="closeModal"
+                  title="Notifications"
+                  description="Renseigne toi sur internet via Google pour savoir si cette information est vraie ou fausse. Vérifie la source de l’information pour pouvoir te faire ton propre avis."
+                  question="L’information est-elle correcte ?"
+                  buttonFirst="Oui"
+                  :buttonFirstAction="updateShare"
+                  buttonSecond="Non"/>
+          </div>
+        </div>
     </div>
 </template>
 <script>
     import Content from '@/components/traps/post/Content';
     import Informations from '@/components/traps/post/Informations';
-    import ButtonContainer from '@/components/traps/post/ButtonContainer';
+    import Modal from '@/components/modal/Modal';
     export default {
         name: "FakeNews",
-        components: { Content, Informations, ButtonContainer},
+        components: { Content, Informations, Modal},
         data: () => ({
+          isModalVisible: false,
         }),
+        methods: {
+            showModal() {
+                this.isModalVisible = true;
+            },
+            closeModal() {
+                this.isModalVisible = false;
+            },
+            updateReport() {
+                this.$store.dispatch('updateAchievement', {type: 'reports', amount: 1})
+            },
+            updateShare() {
+                this.$store.dispatch('updateAchievement', {type: 'shares', amount: 1})
+            }
+        },
     }
 </script>
 
@@ -24,5 +52,20 @@
         padding: 2rem;
         border: 1px solid grey;
         margin-top: 2rem;
+    }
+    .Button {
+      &-container {
+          margin-top: 2rem;
+      }
+
+      &-share {
+          margin-right: 1rem;
+      }
+
+      &-share, &-report {
+          border: 2px solid cornflowerblue;
+          padding: 0.2rem 0.6rem;
+          color: cornflowerblue;
+      }
     }
 </style>
--- client/src/components/traps/Harassment.vue
+++ client/src/components/traps/Harassment.vue
@@ -3,20 +3,32 @@
         <img class="Harassment-image" src="@/assets/logo.png"/>
         <Informations name="Publié par Dylan" like="23" comment="82" date="Il y a 5 heures" />
         <Comment name="Bryan Philipo" date="Il y a 2h" description="Trop moche c'est quoi cette photo !"/>
-        <Comment name="Lucie Bernard" date="Il y a 3 jours" description="Grave je préférais être morte qu’avoir ta tête !"/>
-        <ButtonContainer titleFirst="Commenter" titleSecond="Signaler" />
+        <Comment name="Lucie Bernard" date="Il y a 3 jours" description="Grave je préférerai être morte qu’avoir ta tête !"/>
+        <div class="Button-container">
+          <div class="Button-group">
+            <button class="Button-share" type="button">Commenter</button>
+            <button class="Button-report" type="button" @click="updateReport">Signaler</button>
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
     import Informations from '@/components/traps/post/Informations';
-    import ButtonContainer from '@/components/traps/post/ButtonContainer';
     import Comment from '@/components/traps/post/Comment';
     export default {
         name: "Harassment",
-        components: { Informations, ButtonContainer, Comment},
+        components: { Informations, Comment},
         data: () => ({
         }),
+        methods: {
+            updateReport() {
+                this.$store.dispatch('updateAchievement', {type: 'reports', amount: 1})
+            },
+            updateShare() {
+                this.$store.dispatch('updateAchievement', {type: 'shares', amount: 1})
+            }
+        },
     }
 </script>
 
@@ -28,5 +40,20 @@
     }
     .Informations-container {
       margin-bottom: 1rem;
+    }
+    .Button {
+      &-container {
+          margin-top: 2rem;
+      }
+
+      &-share {
+          margin-right: 1rem;
+      }
+
+      &-share, &-report {
+          border: 2px solid cornflowerblue;
+          padding: 0.2rem 0.6rem;
+          color: cornflowerblue;
+      }
     }
 </style>
