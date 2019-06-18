adeb5d4f0ff7f8e1fc657130f4c285e403908f58
amelie laurent <amelielaurent38@gmail.com>
2019-06-18 19:10:14 +0200

    Start summary page

--- client/src/views/student/StudentSummary.vue
+++ client/src/views/student/StudentSummary.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        <p>Bilan de l'expérience</p>
-        <p>gg tu es  à la fin</p>
+    <div class="summary__container">
+        <div class="summary__step">
+            <hr class="summary__border"/><div class="summary__circle"></div>
+        </div>
+        <ul class="summary__datas">
+            <li class="score">{{points}} point{{isPlurial('points')}}</li>
+            <li class="share">{{shares}} partage{{isPlurial('shares')}}</li>
+            <li class="report">{{reports}} signalement{{isPlurial('reports')}}</li>
+        </ul>
+        <div class="summary__content">
+            <h1>85 Wow !</h1>
+            <p>Tu es un petit expert des réseaux sociaux ! Tu es extrêmement attentif aux contenus qui te sont présentés. Nous ne pouvons que t'encourager à continuer d'avoir cette démarche positive sur internet :)</p>
+        </div>
     </div>
 </template>
 
@@ -9,9 +19,96 @@
     export default {
         name: 'StudentSummary',
         components: {
+        },
+        computed: {
+            points() {
+                return this.$store.state.score.points;
+            },
+            shares() {
+                return this.$store.state.score.shares;
+            },
+            reports() {
+                return this.$store.state.score.reports;
+            },
+            isPlurial() {
+                return type => this.$store.state.score[type] > 1 ? 's' : null;
+            }
         }
     }
 </script>
 
 <style scoped lang="scss">
+    .summary {
+        &__container {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            height: 100%;
+        }
+        &__content {
+            max-width: 50rem;
+            &:after {
+                content: "";
+                background-color: $blue;
+                width: 470px;
+                height: 39rem;
+                color: $white;
+                position: absolute;
+                border-radius: 0.8rem;
+                transform: rotate(-19deg);
+                
+            }
+        }
+        &__datas {
+            color: $blue;
+            font-size: 2.2rem;
+            li {
+                margin-top: 7rem;
+                border-bottom: 1px solid $blue;
+                width: 18rem;
+                position: relative;
+                &:first-child {
+                    margin-top: 0;
+                }
+                &:after { 
+                    content: "\26AC";
+                    position: absolute;
+                    right: -1.5rem;
+                    margin-top: 1.5rem;
+                }  
+            }
+        }
+        &__step {
+            position: relative;
+            display: flex;
+            align-items: center;
+        }
+        &__circle {
+            background: $violet;
+            width: 15rem;
+            height: 15rem;
+            border-radius: 100%;
+            &:after {
+                content: "";
+                width: 18rem;
+                height: 18rem;
+                border: 2px solid $violet;
+                position: absolute;
+                border-radius: 100%;
+                right: -16px;
+                top: -14px;
+            }
+            &:before {
+                content: "";
+                width: 10rem;
+                height: 2px;
+                background: $violet;
+            }
+        }
+        &__border {
+            width: 10rem;
+            height: 3px;
+            border-top: 3px dashed $violet;
+        }
+    }
 </style>
