e2ce06732a5cb0a45f6c1222c1baf2c5430641cb
Sebastien Hernoux <seb_her@hotmail.fr>
2019-06-19 13:27:23 +0200

    create trap info component

---
+++ client/src/components/dashboard/TrapInfo/TrapInfoContainer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div
+            class="trap-progress"
+            v-for="(step, i) in steps"
+            :key="i">
+            <span class="trap__name">{{step.name}}</span>
+            <span class="trap__number">{{step.number}}</span>
+            <v-progress-linear v-model="step.progress"></v-progress-linear>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TrapInfoContainer",
+        data: () => ({
+            steps: [{
+                name: 'Etape 1',
+                progress: 0,
+                number: 0
+            },{
+                name: 'Etape 2',
+                progress: 0,
+                number: 0
+            },{
+                name: 'Etape 3',
+                progress: 0,
+                number: 0
+            }]
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
